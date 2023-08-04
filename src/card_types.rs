dcd5505eb240a896aae85edead38433c29462a7d
OLoKo64 <naldoepingo1@gmail.com>
2023-08-03 23:29:47 -0300

    feat: improve code

--- src/card_types.rs
+++ src/card_types.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, ops::DerefMut};
+use std::collections::HashMap;
 
 pub(crate) struct CardTypes(HashMap<&'static str, CardTypeInner>);
 
@@ -10,14 +10,14 @@ impl CardTypes {
                 CardTypeInner {
                     nice_type: "Visa",
                     type_: "visa",
-                    patterns: &[&[4]],
+                    patterns: &[&["4"]],
                     gaps: &[4, 8, 12],
                     lengths: &[16, 18, 19],
                     code: Code {
                         name: "CVV",
                         size: 3,
                     },
-                    match_strength: None,
+                    match_strength: 0,
                 },
             ),
             (
@@ -26,12 +26,12 @@ impl CardTypes {
                     nice_type: "Mastercard",
                     type_: "mastercard",
                     patterns: &[
-                        &[51, 55],
-                        &[2221, 2229],
-                        &[223, 229],
-                        &[23, 26],
-                        &[270, 271],
-                        &[2720],
+                        &["51", "55"],
+                        &["2221", "2229"],
+                        &["223", "229"],
+                        &["23", "26"],
+                        &["270", "271"],
+                        &["2720"],
                     ],
                     gaps: &[4, 8, 12],
                     lengths: &[16],
@@ -39,7 +39,7 @@ impl CardTypes {
                         name: "CVC",
                         size: 3,
                     },
-                    match_strength: None,
+                    match_strength: 0,
                 },
             ),
             (
@@ -47,14 +47,14 @@ impl CardTypes {
                 CardTypeInner {
                     nice_type: "American Express",
                     type_: "american-express",
-                    patterns: &[&[34], &[37]],
+                    patterns: &[&["34"], &["37"]],
                     gaps: &[4, 10],
                     lengths: &[15],
                     code: Code {
                         name: "CID",
                         size: 4,
                     },
-                    match_strength: None,
+                    match_strength: 0,
                 },
             ),
             (
@@ -62,14 +62,14 @@ impl CardTypes {
                 CardTypeInner {
                     nice_type: "Diners Club",
                     type_: "diners-club",
-                    patterns: &[&[300, 305], &[36], &[38], &[39]],
+                    patterns: &[&["300", "305"], &["36"], &["38"], &["39"]],
                     gaps: &[4, 10],
                     lengths: &[14, 16, 19],
                     code: Code {
                         name: "CVV",
                         size: 3,
                     },
-                    match_strength: None,
+                    match_strength: 0,
                 },
             ),
             (
@@ -77,14 +77,14 @@ impl CardTypes {
                 CardTypeInner {
                     nice_type: "Discover",
                     type_: "discover",
-                    patterns: &[&[6011], &[644, 649], &[65]],
+                    patterns: &[&["6011"], &["644", "649"], &["65"]],
                     gaps: &[4, 8, 12],
                     lengths: &[16, 19],
                     code: Code {
                         name: "CID",
                         size: 3,
                     },
-                    match_strength: None,
+                    match_strength: 0,
                 },
             ),
             (
@@ -92,14 +92,14 @@ impl CardTypes {
                 CardTypeInner {
                     nice_type: "JCB",
                     type_: "jcb",
-                    patterns: &[&[2131], &[1800], &[3528, 3589]],
+                    patterns: &[&["2131"], &["1800"], &["3528", "3589"]],
                     gaps: &[4, 8, 12],
                     lengths: &[16, 17, 18, 19],
                     code: Code {
                         name: "CVV",
                         size: 3,
                     },
-                    match_strength: None,
+                    match_strength: 0,
                 },
             ),
             (
@@ -108,28 +108,28 @@ impl CardTypes {
                     nice_type: "UnionPay",
                     type_: "unionpay",
                     patterns: &[
-                        &[620],
-                        &[62100, 62182],
-                        &[62184, 62187],
-                        &[62185, 62197],
-                        &[62200, 62205],
-                        &[622010, 622999],
-                        &[622018],
-                        &[62207, 62209],
-                        &[623, 626],
-                        &[6270],
-                        &[6272],
-                        &[6276],
-                        &[627700, 627779],
-                        &[627781, 627799],
-                        &[6282, 6289],
-                        &[6291],
-                        &[6292],
-                        &[810],
-                        &[8110, 8131],
-                        &[8132, 8151],
-                        &[8152, 8163],
-                        &[8164, 8171],
+                        &["620"],
+                        &["62100", "62182"],
+                        &["62184", "62187"],
+                        &["62185", "62197"],
+                        &["62200", "62205"],
+                        &["622010", "622999"],
+                        &["622018"],
+                        &["62207", "62209"],
+                        &["623", "626"],
+                        &["6270"],
+                        &["6272"],
+                        &["6276"],
+                        &["627700", "627779"],
+                        &["627781", "627799"],
+                        &["6282", "6289"],
+                        &["6291"],
+                        &["6292"],
+                        &["810"],
+                        &["8110", "8131"],
+                        &["8132", "8151"],
+                        &["8152", "8163"],
+                        &["8164", "8171"],
                     ],
                     gaps: &[4, 8, 12],
                     lengths: &[14, 15, 16, 17, 18, 19],
@@ -137,7 +137,7 @@ impl CardTypes {
                         name: "CVN",
                         size: 3,
                     },
-                    match_strength: None,
+                    match_strength: 0,
                 },
             ),
             (
@@ -146,14 +146,14 @@ impl CardTypes {
                     nice_type: "Maestro",
                     type_: "maestro",
                     patterns: &[
-                        &[493698],
-                        &[500000, 504174],
-                        &[504176, 506698],
-                        &[506779, 508999],
-                        &[56, 59],
-                        &[63],
-                        &[67],
-                        &[6],
+                        &["493698"],
+                        &["500000", "504174"],
+                        &["504176", "506698"],
+                        &["506779", "508999"],
+                        &["56", "59"],
+                        &["63"],
+                        &["67"],
+                        &["6"],
                     ],
                     gaps: &[4, 8, 12],
                     lengths: &[12, 13, 14, 15, 16, 17, 18, 19],
@@ -161,7 +161,7 @@ impl CardTypes {
                         name: "CVC",
                         size: 3,
                     },
-                    match_strength: None,
+                    match_strength: 0,
                 },
             ),
             (
@@ -170,31 +170,31 @@ impl CardTypes {
                     nice_type: "Elo",
                     type_: "elo",
                     patterns: &[
-                        &[401178],
-                        &[401179],
-                        &[438935],
-                        &[457631],
-                        &[457632],
-                        &[431274],
-                        &[451416],
-                        &[457393],
-                        &[504175],
-                        &[506699, 506778],
-                        &[509000, 509999],
-                        &[627780],
-                        &[636297],
-                        &[636368],
-                        &[650031, 650033],
-                        &[650035, 650051],
-                        &[650405, 650439],
-                        &[650485, 650538],
-                        &[650541, 650598],
-                        &[650700, 650718],
-                        &[650720, 650727],
-                        &[650901, 650978],
-                        &[651652, 651679],
-                        &[655000, 655019],
-                        &[655021, 655058],
+                        &["401178"],
+                        &["401179"],
+                        &["438935"],
+                        &["457631"],
+                        &["457632"],
+                        &["431274"],
+                        &["451416"],
+                        &["457393"],
+                        &["504175"],
+                        &["506699", "506778"],
+                        &["509000", "509999"],
+                        &["627780"],
+                        &["636297"],
+                        &["636368"],
+                        &["650031", "650033"],
+                        &["650035", "650051"],
+                        &["650405", "650439"],
+                        &["650485", "650538"],
+                        &["650541", "650598"],
+                        &["650700", "650718"],
+                        &["650720", "650727"],
+                        &["650901", "650978"],
+                        &["651652", "651679"],
+                        &["655000", "655019"],
+                        &["655021", "655058"],
                     ],
                     gaps: &[4, 8, 12],
                     lengths: &[16],
@@ -202,7 +202,7 @@ impl CardTypes {
                         name: "CVE",
                         size: 3,
                     },
-                    match_strength: None,
+                    match_strength: 0,
                 },
             ),
             (
@@ -210,14 +210,14 @@ impl CardTypes {
                 CardTypeInner {
                     nice_type: "Mir",
                     type_: "mir",
-                    patterns: &[&[2200, 2204]],
+                    patterns: &[&["2200", "2204"]],
                     gaps: &[4, 8, 12],
                     lengths: &[16, 17, 18, 19],
                     code: Code {
                         name: "CVP2",
                         size: 3,
                     },
-                    match_strength: None,
+                    match_strength: 0,
                 },
             ),
             (
@@ -226,13 +226,13 @@ impl CardTypes {
                     nice_type: "Hiper",
                     type_: "hiper",
                     patterns: &[
-                        &[637095],
-                        &[63737423],
-                        &[63743358],
-                        &[637568],
-                        &[637599],
-                        &[637609],
-                        &[637612],
+                        &["637095"],
+                        &["63737423"],
+                        &["63743358"],
+                        &["637568"],
+                        &["637599"],
+                        &["637609"],
+                        &["637612"],
                     ],
                     gaps: &[4, 8, 12],
                     lengths: &[16],
@@ -240,7 +240,7 @@ impl CardTypes {
                         name: "CVC",
                         size: 3,
                     },
-                    match_strength: None,
+                    match_strength: 0,
                 },
             ),
             (
@@ -248,14 +248,14 @@ impl CardTypes {
                 CardTypeInner {
                     nice_type: "Hipercard",
                     type_: "hipercard",
-                    patterns: &[&[606282]],
+                    patterns: &[&["606282"]],
                     gaps: &[4, 8, 12],
                     lengths: &[16],
                     code: Code {
                         name: "CVC",
                         size: 3,
                     },
-                    match_strength: None,
+                    match_strength: 0,
                 },
             ),
         ];
@@ -269,39 +269,40 @@ impl CardTypes {
     }
 
     pub fn get_credit_card_type(
-        &mut self,
+        &self,
         card_number: impl AsRef<str>,
-    ) -> Option<Vec<CardTypeInner>> {
+    ) -> Result<Vec<CardTypeInner>, std::num::ParseIntError> {
         let card_number = card_number.as_ref();
 
         if card_number.is_empty() {
-            return None;
+            return Ok(vec![]);
         }
 
         let mut results = Vec::new();
 
         let mut all_cards = self.get_all_card_types();
 
-        all_cards.iter_mut().for_each(|card_type| {
-            add_best_match_to_results(card_number, card_type, &mut results);
-        });
+        for card_type in all_cards.iter_mut() {
+            add_best_match_to_results(card_number, card_type, &mut results)?
+        }
 
         let best_match = find_best_match(&mut results);
 
         if let Some(best_match) = best_match {
-            return Some(vec![best_match]);
+            return Ok(vec![best_match]);
         }
 
-        Some(
-            results
-                .iter()
-                .map(|card_type| (*card_type).clone())
-                .collect(),
-        )
+        Ok(results
+            .iter()
+            .map(|card_type| (*card_type).clone())
+            .collect())
     }
 
-    pub fn get_all_card_types(&mut self) -> Vec<&mut CardTypeInner> {
-        self.0.values_mut().collect()
+    pub fn get_all_card_types(&self) -> Vec<CardTypeInner> {
+        self.0
+            .values()
+            .map(|card_type| (*card_type).clone())
+            .collect()
     }
 }
 
@@ -312,13 +313,13 @@ fn find_best_match<'a>(results: &'a mut [&mut CardTypeInner]) -> Option<CardType
 
     let mut best_match_result = None;
 
-    results.iter_mut().for_each(|card_type| {
+    for card_type in results.iter_mut() {
         if best_match_result.is_none() {
             best_match_result = Some(card_type);
-        } else if card_type.match_strength > best_match_result.as_ref().unwrap().match_strength {
+        } else if card_type.match_strength > best_match_result.as_ref()?.match_strength {
             best_match_result = Some(card_type);
         }
-    });
+    }
 
     best_match_result.map(|card_type| (*card_type).clone())
 }
@@ -326,7 +327,7 @@ fn find_best_match<'a>(results: &'a mut [&mut CardTypeInner]) -> Option<CardType
 fn can_determine_best_match(results: &[&mut CardTypeInner]) -> bool {
     let number_of_results_with_max_strength = results
         .iter()
-        .filter(|card_type| card_type.match_strength > Some(1))
+        .filter(|card_type| card_type.match_strength > 1)
         .count();
 
     number_of_results_with_max_strength > 1 && number_of_results_with_max_strength == results.len()
@@ -336,34 +337,36 @@ fn add_best_match_to_results<'a, 'b>(
     card_number: &str,
     card_type: &'b mut CardTypeInner,
     results: &'a mut Vec<&'b mut CardTypeInner>,
-) {
+) -> Result<(), std::num::ParseIntError> {
     use crate::utils::matches;
 
     for pattern in card_type.patterns.iter() {
-        if !matches(card_number, pattern) {
+        if !matches(card_number, pattern)? {
             continue;
         }
 
         let pattern_length = pattern.len();
 
         if card_number.len() >= pattern_length {
-            card_type.match_strength = Some(pattern_length as i32);
+            card_type.match_strength = pattern_length as u32;
         }
 
         results.push(card_type);
         break;
     }
+
+    Ok(())
 }
 
 #[derive(Debug, Clone)]
 pub struct CardTypeInner {
     pub nice_type: &'static str,
     pub type_: &'static str,
-    pub patterns: &'static [&'static [i32]],
+    pub patterns: &'static [&'static [&'static str]],
     pub gaps: &'static [i32],
     pub lengths: &'static [i32],
     pub code: Code,
-    pub match_strength: Option<i32>,
+    pub match_strength: u32,
 }
 
 #[derive(Debug, Clone)]
--- src/utils.rs
+++ src/utils.rs
@@ -1,28 +1,33 @@
-pub(crate) fn matches(card_number: &str, pattern: &'static [i32]) -> bool {
+pub(crate) fn matches(
+    card_number: &str,
+    pattern: &'static [&'static str],
+) -> Result<bool, std::num::ParseIntError> {
     if pattern.len() == 1 {
-        return matches_pattern(card_number, pattern[0]);
+        return Ok(matches_pattern(card_number, pattern[0]));
     }
 
-    match_range(card_number, pattern[0], pattern[1])
+    Ok(match_range(card_number, pattern[0], pattern[1])?)
 }
 
-fn matches_pattern(card_number: &str, pattern: i32) -> bool {
-    let pattern = pattern.to_string();
-
+fn matches_pattern(card_number: &str, pattern: &'static str) -> bool {
     pattern.get(..card_number.len()).unwrap_or(&pattern)
         == card_number.get(..pattern.len()).unwrap_or(&card_number)
 }
 
-fn match_range(card_number: &str, min: i32, max: i32) -> bool {
-    let mut max_len_to_check = max.to_string().len();
+fn match_range(
+    card_number: &str,
+    min: &'static str,
+    max: &'static str,
+) -> Result<bool, std::num::ParseIntError> {
+    let mut max_len_to_check = max.len();
     if max_len_to_check > card_number.len() {
         max_len_to_check = card_number.len();
     }
     let str_slice = &card_number[..max_len_to_check];
-    let int_repr = str_slice.parse::<i32>().unwrap();
+    let int_repr = str_slice.parse::<i32>()?;
 
-    let min = &min.to_string()[..str_slice.len()];
-    let max = &max.to_string()[..str_slice.len()];
+    let min = &min[..str_slice.len()];
+    let max = &max[..str_slice.len()];
 
-    int_repr >= min.parse::<i32>().unwrap() && int_repr <= max.parse::<i32>().unwrap()
+    Ok(int_repr >= min.parse::<i32>()? && int_repr <= max.parse::<i32>()?)
 }
